0aebb17e99650c6560fe5aefb649afcd3e1d4bb8
Geralt555 <70748374+Geralt555@users.noreply.github.com>
2021-02-13 11:44:43 +0300

    Update exem.py

--- exem.py
+++ exem.py
@@ -1,23 +1,3 @@
-class Liquid:
-    '''Жидкость'''
-
-    def __init__(self, density: float = 0, name: str = 'liquid'):
-        '''density - плотность, name - название'''
-        self._density, self._name = density, name  # начальные значения
-
-    density, name = property(), property()
-
-    @density.setter
-    def density(self, value: float):
-        '''Установка значения плотности'''
-        self._density = value
-
-    @name.setter
-    def name(self, value: str):
-        '''Установка имени'''
-        self._name = value
-
-
 class Alcohol(Liquid):
     '''Алкоголь'''
 
